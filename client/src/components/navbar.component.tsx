9a5792749f4c3e32a7d4070ac3c26aa5abdee172
Mohit Raut <mohitraut.21@stvincentngp.edu.in>
2024-02-05 15:27:23 +0530

    Add react-icons and update navbar and footer components

--- client/src/components/navbar.component.tsx
+++ client/src/components/navbar.component.tsx
@@ -1,10 +1,29 @@
-const Navbar = () => {
+import React from "react";
+// import { Button } from "../ui/button";
+import Link from "next/link";
+
+function Header() {
   return (
-    <header className="flex top-0 z-[-9] h-[72px]   justify-between bg-black px-8 py-5">
-      <div>
-        <h1 className="text-3xl font-bold text-white">Fresh Flow</h1>
+    <div className="flex bg-black h-24 w-full items-center justify-between bg-primary ">
+      <div className="mx-3">
+        <button>
+          <h1 className="text-3xl font-bold text-white"><Link href="/">Fresh Flow</Link></h1>
+        </button>
       </div>
-    </header>
+      <div className="mx-5 flex justify-evenly">
+        <span className="mx-3">
+          <button  className="text-white text-primary hover:bg-secondary">
+            <Link href="/login">Register</Link>
+          </button>
+        </span>
+        <span className="mx-3">
+          <button  className="text-white text-primary hover:bg-secondary">
+            <Link href="/login">Login</Link>
+          </button>
+        </span>
+      </div>
+    </div>
   );
-};
-export default Navbar;
+}
+
+export default Header;
