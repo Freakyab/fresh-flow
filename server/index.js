bd84f3d6096460ec46c085dd2dfd96ed39fe6564
Freakyab <aryanbhisikar1@gmail.com>
2024-04-14 17:45:56 +0530

    fix

--- server/index.js
+++ server/index.js
@@ -5,11 +5,7 @@ const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 5000;
 const app = express();
 
-app.use(bodyParser.json());
-
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*")
-}) 
+app.use(bodyParser.json()); 
 app.use(cors());
 app.use(express.json());
 
